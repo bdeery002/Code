e6eafd950ed28f62188401ed895325a38c59d1f1
bozobono <156530572+bozobono@users.noreply.github.com>
2026-01-17 20:47:30 +0000

    Fixing djangotutorial structure in main repo

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -138,5 +138,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.github.dev",
     "https://localhost:8080",
     "http://localhost:8080",
-    "http://127.0.0.1:8080"
+    "http://127.0.0.1:8080",
+    'http://localhost:8081',
+    'https://localhost:8081'
 ]
