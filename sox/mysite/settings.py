a0eb9e2b9363168ef6bfa00cfa37ffd09b30ae09
Brendan Deery <156530572+bdeery002@users.noreply.github.com>
2026-08-07 00:00:01 +0000

    update

--- sox/mysite/settings.py
+++ sox/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 import dj_database_url
 import os
@@ -19,6 +20,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
